1894b08be3b99c3a1731e961bfbefef020c56461
davnah <davnah.payne@ips.unibe.ch>
2022-01-14 14:24:39 +0100

    commit

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import * as d3 from 'd3'
+
 
 @Component({
   selector: 'app-root',
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,11 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BarComponent } from './bar/bar.component';
+import { PieComponent } from './pie/pie.component';
+import { ScatterComponent } from './scatter/scatter.component';
+import { LineChartComponent } from './line-chart/line-chart.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    BarComponent
+    BarComponent,
+    PieComponent,
+    ScatterComponent,
+    LineChartComponent,
   ],
   imports: [
     BrowserModule,
